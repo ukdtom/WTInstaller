351754ae892d3613941ce6c111438ef4a0b40666
Mikael Aspehed <dagalufh@gmail.com>
2016-07-18 20:06:01 +0200

    Added Windows Easy Installer

---
+++ src/Windows/WebTools Easy Installer/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WebTools_Easy_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TargetInstallDir = new System.Windows.Forms.Label();
+            this.LogBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.StatusBar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Installation Dir:";
+            // 
+            // TargetInstallDir
+            // 
+            this.TargetInstallDir.AutoSize = true;
+            this.TargetInstallDir.Location = new System.Drawing.Point(94, 9);
+            this.TargetInstallDir.Name = "TargetInstallDir";
+            this.TargetInstallDir.Size = new System.Drawing.Size(0, 13);
+            this.TargetInstallDir.TabIndex = 3;
+            // 
+            // LogBox
+            // 
+            this.LogBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LogBox.Location = new System.Drawing.Point(0, 149);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ReadOnly = true;
+            this.LogBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogBox.Size = new System.Drawing.Size(618, 241);
+            this.LogBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "GitHub Link:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(94, 22);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(304, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/dagalufh/WebTools.bundle/releases/latest";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Currently installed version:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Version available on GitHub:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(159, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "<Current Version>";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(159, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "<Latest Available>";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(12, 119);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(593, 23);
+            this.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.ProgressBar.TabIndex = 13;
+            this.ProgressBar.Visible = false;
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Location = new System.Drawing.Point(12, 93);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.ReadOnly = true;
+            this.StatusBar.Size = new System.Drawing.Size(593, 20);
+            this.StatusBar.TabIndex = 14;
+            this.StatusBar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 390);
+            this.Controls.Add(this.StatusBar);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.TargetInstallDir);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebTools Easy Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TargetInstallDir;
+        private System.Windows.Forms.TextBox LogBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.Windows.Forms.TextBox StatusBar;
+    }
+}
+
---
+++ src/Windows/WebTools Easy Installer/Form1.cs
@@ -0,0 +1,243 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.IO.Compression;
+
+namespace WebTools_Easy_Installer
+{
+    public partial class Form1 : Form
+    {
+        const string GitHubLatestInformation = "https://api.github.com/repos/dagalufh/webtools.bundle/releases/latest";
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddToLog("Application started.");
+           
+            // Lets start by checking if Plex Media Server is installed or not.
+            // We do this by checking that the registry value for Plugin-Dir is existing.
+            
+            RegistryKey RegCU = Registry.CurrentUser;
+            RegistryKey Plex = RegCU.OpenSubKey("Software\\Plex, Inc.\\Plex Media Server", false);
+
+            AddToLog("Checking for Plex Media Server...");
+            if (Plex != null)
+            {
+                AddToLog("Found Plex in registry (HKCU\\Software\\Plex, Inc.\\Plex Media Server).");
+                string PlexPluginDir = Environment.GetEnvironmentVariable("LOCALAPPDATA") + "\\Plex Media Server\\Plug-ins";
+                try
+                {
+                    PlexPluginDir = Plex.GetValue("LocalAppDataPath").ToString() + "\\Plex Media Server\\Plug-ins";
+                    AddToLog("Found custom PluginDir. (" + PlexPluginDir + ")");
+                } catch
+                {
+                    AddToLog("Using default PluginDir. (" + PlexPluginDir + ")");
+                    // No custom path set. Using default.
+                    //MessageBox.Show("Error occured while fetching registry value: " + e.Message);
+                }
+
+                TargetInstallDir.Text = PlexPluginDir;
+                //MessageBox.Show("Target Plugin path: " + PlexPluginDir);
+
+
+                // Check if WebTools is already installed.
+                if (Directory.Exists(PlexPluginDir + "\\WebTools.Bundle") == true) {
+                    AddToLog("Previous WebTools installation found.");
+
+                    // Check what version is installed.
+                    if (File.Exists(PlexPluginDir + "\\WebTools.Bundle\\VERSION") == true)
+                    {
+                        string[] lines = File.ReadAllLines(PlexPluginDir + "\\WebTools.Bundle\\VERSION");
+                        foreach (string line in lines)
+                        {
+                            if (line.Length > 0)
+                            {
+                                label6.Text = line;
+                                break;
+                            }
+                        }
+                    }
+                    // Checking if __init__.py contains VERSION = 
+                    else if (File.Exists(PlexPluginDir + "\\WebTools.Bundle\\Contents\\Code\\__init__.py") == true)
+                    {
+                        string[] lines = File.ReadAllLines(PlexPluginDir + "\\WebTools.Bundle\\Contents\\Code\\__init__.py");
+                        foreach (string line in lines)
+                        {
+                            if (line.IndexOf("VERSION = ") != -1)
+                            {
+                                char[] chars = { '=' };   
+                                string[] version = line.Split(chars);
+                                version[1] = version[1].ToString().Trim();
+
+                                label6.Text = version[1].Substring(1,version[1].Length -2);
+
+                                break;
+                            }
+                        }
+                    } else
+                    {
+                        label6.Text = "-";
+                        AddToLog("Unable to locate __init__.py or VERSION to determine installed version. Might be a corrupt installation.");
+                    }
+                    
+
+                    
+
+                } else
+                {
+                    AddToLog("No previous WebTools installation found.");
+                    label6.Text = "No previous installation found.";
+                }
+
+                // Check what version is available from GitHub
+                HttpWebRequest GitHubWebClient =(HttpWebRequest)HttpWebRequest.Create(GitHubLatestInformation);
+                GitHubWebClient.Method = "GET";
+                GitHubWebClient.UserAgent = "Custom";
+                try
+                {
+                    // Fetch the information about the latest release from github
+                    HttpWebResponse response = (HttpWebResponse)GitHubWebClient.GetResponse();
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    string TEST= reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                    
+                    // Split on " since it's one long JSON string.
+                    string[] lines = TEST.Split(new string[] { "\"" }, StringSplitOptions.None);
+                    for (var i = 0; i < lines.Length; i++)
+                    {
+
+                        // Look for tag_name, this contains the version number
+                        if (lines[i].IndexOf("tag_name") != -1)
+                        {
+                            label7.Text = lines[i + 2];
+                        }
+
+                        // Look for the download link to be used when downloading the plugin.
+                        if ( (lines[i].IndexOf("browser_download_url") != -1) && (lines[i +2].IndexOf("WebTools.bundle.zip") != -1) )
+                        {
+                            linkLabel1.Text = lines[i + 2];
+                        }
+                        
+                    }
+
+                } catch (Exception e)
+                {
+                    MessageBox.Show("Error: " + e.Message);
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Plex Media Server does not seem to be installed.");
+                AddToLog("Plex Media server not found. Make sure it's installed first.");
+                this.Close();
+            }
+
+        }
+
+        // Unified logfunction
+        private void AddToLog( string Message)
+        {
+            LogBox.AppendText(Message + Environment.NewLine);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Installation function
+            StatusBar.Visible = true;
+            ProgressBar.Visible = true;
+            ProgressBar.Value = 0;
+            AddToLog("Starting Installation");
+            string TemporaryFilename = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\WebTools.Bundle.Latest.zip";
+
+            if (File.Exists(TemporaryFilename) == true) {
+                AddToLog("Removing old installation zip.");
+                File.Delete(TemporaryFilename);
+
+            }
+            
+             try
+            {
+                StatusBar.Text = "";
+                AddToLog("Starting download of: " + linkLabel1.Text);
+                AddToLog("Temporary target: " + TemporaryFilename);
+
+                WebClient wc = new WebClient();
+                wc.Headers.Add("user-agent", "custom");
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                wc.DownloadFileAsync(new System.Uri(linkLabel1.Text), TemporaryFilename);
+ 
+            } catch (Exception wce)
+            {
+                MessageBox.Show("Error: " + wce.Message);
+            }
+
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            StatusBar.Text = "";
+            StatusBar.Visible = false;
+            ProgressBar.Visible = false;
+            AddToLog("Download completed.");
+            UnPack();
+
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            StatusBar.Text = "Downloading WebTools.Bundle.Zip: " + e.ProgressPercentage + " completed.";
+            ProgressBar.Value = e.ProgressPercentage;
+        }
+
+        void UnPack()
+        {
+            if (Directory.Exists(TargetInstallDir.Text + "\\WebTools.Bundle") == true)
+            {
+                AddToLog("Will now delete previous WebTools installation folder: " + TargetInstallDir.Text + "\\WebTools.Bundle");
+
+                try
+                {
+                    Directory.Delete(TargetInstallDir.Text + "\\WebTools.Bundle", true);
+                    AddToLog("Successfully deleted old folder: " + TargetInstallDir.Text + "\\WebTools.Bundle");
+                } catch
+                {
+                    AddToLog("Failed to delete old folder: " + TargetInstallDir.Text + "\\WebTools.Bundle");
+                    AddToLog("Make sure it's not in use.");
+                }
+            }
+            AddToLog("Starting unpacking.");
+            ZipArchive archive = ZipFile.Open(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\WebTools.Bundle.Latest.zip", ZipArchiveMode.Read);
+            try
+            {
+                archive.ExtractToDirectory(TargetInstallDir.Text);
+                AddToLog("Unpacking completed.");
+            } catch (Exception uc)
+            {
+                AddToLog("An error occured while unpacking: " + uc.Message);
+            }
+
+            // Close Zip-file, unlocking it for the thread to remove it.
+            archive.Dispose();
+
+            AddToLog("Deleting zip file: " + Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\WebTools.Bundle.Latest.zip");
+            File.Delete(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\WebTools.Bundle.Latest.zip");
+
+            AddToLog("Installation Complete.");
+            MessageBox.Show("Installation complete.");
+        }  
+
+       
+    }
+}
